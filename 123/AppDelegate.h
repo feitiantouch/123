8bf97638ae495662c560f602180560018ac05e13
feitiantouch <feitiantouch@163.com>
2015-12-10 21:08:38 +0800

    1
    1

---
+++ 123/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  123
+//
+//  Created by feitian on 15/12/10.
+//  Copyright © 2015年 com.Ideal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 123/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  123
+//
+//  Created by feitian on 15/12/10.
+//  Copyright © 2015年 com.Ideal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
